db6ea23da18d486dc162f91af0d4220d163ab669
Brandon Williams <BrandonWilliamsCS@gmail.com>
2020-07-07 16:01:59 -0400

    Convert CompoundStateManager to CompoundStateGenerator

---
+++ src/lib/system/state/CompoundStateGenerator.spec.js
@@ -0,0 +1,50 @@
+import { StateGenerator } from "../../kernel/StateGenerator";
+import { CompoundStateGenerator } from "./CompoundStateGenerator";
+
+describe("CompoundStateGenerator", () => {
+  describe("apply", () => {
+    it("combines the result of its constituent StateGenerators' `apply` calls", async () => {
+      // Arrange
+      const subStateGeneratorMap = {
+        component1: new TestStateGenerator("component1"),
+        component2: new TestStateGenerator("component2"),
+      };
+      const stateGenerator = new CompoundStateGenerator(subStateGeneratorMap);
+      const action = { type: "ACTION" };
+
+      // Act
+      const result = await stateGenerator.apply(action, undefined);
+
+      // Assert
+      expect(result).toMatchObject({
+        component1: {
+          identifier: "component1",
+          forAction: action,
+        },
+        component2: {
+          identifier: "component2",
+          forAction: action,
+        },
+      });
+    });
+  });
+});
+
+class TestStateGenerator extends StateGenerator {
+  constructor(identifier) {
+    super();
+    this.identifier = identifier;
+    this.initialState = {
+      identifier: this.identifier,
+      forAction: undefined,
+      prevState: undefined,
+    };
+  }
+  apply(action, prevState) {
+    return Promise.resolve({
+      identifier: this.identifier,
+      forAction: action,
+      prevState,
+    });
+  }
+}
