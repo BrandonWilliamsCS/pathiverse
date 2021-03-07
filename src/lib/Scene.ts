5c4da051bf03dcb2f762af5488ebecec1eff9323
Brandon Williams <BrandonWilliamsCS@gmail.com>
2021-03-07 10:43:33 -0500

    Clear legacy files

--- src/lib/Scene.ts
+++ src/lib/Scene.ts
@@ -1,15 +1,7 @@
-import { Action } from "./Action";
-import { ContentIndicator } from "./Content";
-
 /**
  * A descriptive overview of a single, discrete element of a Story within a World.
  */
 export interface Scene {
-  world: string;
   story: string;
   name: string;
-  summary: string | undefined;
-  branchSummary: string | undefined;
-  contentIndicator: ContentIndicator;
-  possibleActions: Action[];
 }
--- src/lib/kernel/StateGenerator.ts
+++
@@ -1,9 +0,0 @@
-import { Action } from "../Action";
-import { State } from "../State";
-
-/**
- * A reducer for generating state by applying an action to previously accumulated state.
- */
-export abstract class StateGenerator<S extends State> {
-  public abstract apply(action: Action, prevState: S | undefined): Promise<S>;
-}
