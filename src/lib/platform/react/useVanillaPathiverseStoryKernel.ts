9f89f0a92f6e00a2ff0f5dfa1681ebf7c9a4fda4
Brandon Williams <BrandonWilliamsCS@gmail.com>
2021-03-07 10:52:32 -0500

    add convenience hook for no-user-state cases

---
+++ src/lib/platform/react/useVanillaPathiverseStoryKernel.ts
@@ -0,0 +1,21 @@
+import { Action } from "lib/Action";
+import { Scene } from "lib/Scene";
+import { encapsulateStoryReducer } from "lib/kernel/story/encapsulateStoryReducer";
+import { Story } from "lib/kernel/story/Story";
+import { StoryState } from "lib/kernel/story/StoryState";
+import { ActionHandler } from "lib/kernel/ActionHandler";
+
+/**
+ * Encapsulate a Pathiverse story reducer into React state with updates based on actions and ignoring user state.
+ * @param story - represents story specification, particularly the initial scene.
+ * @typeParam S - Describes a scene within the contextual story.
+ * @typeParam A - describes possible actions.
+ */
+export function useVanillaPathiverseStoryKernel<
+  S extends Scene,
+  A extends Action
+>(story: Story<S>): [StoryState<S, undefined>, ActionHandler<A>] {
+  return encapsulateStoryReducer(story, vanillaUserStateReducer, undefined);``
+}
+
+const vanillaUserStateReducer = () => Promise.resolve(undefined);
