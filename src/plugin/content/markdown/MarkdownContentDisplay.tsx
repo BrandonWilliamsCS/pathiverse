6ad73a2707260b1d3d77a693dcd0be80670aced3
Brandon Williams <BrandonWilliamsCS@gmail.com>
2022-06-12 13:35:59 -0400

    Translate markdown content with react-markdown

--- src/plugin/content/markdown/MarkdownContentDisplay.tsx
+++ src/plugin/content/markdown/MarkdownContentDisplay.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
 import { MarkdownContent } from "./MarkdownContent";
 
@@ -6,9 +7,9 @@ export interface MarkdownContentDisplayProps {
   content: MarkdownContent;
 }
 
-/** Displays "markdown" content by simply rendering the text directly. */
-export const MarkdownContentDisplay: React.FC<
-  MarkdownContentDisplayProps
-> = ({ content }) => {
-  return <>{content.value}</>;
+/** Displays "markdown" content by simply rendering the text directly. */ //!!
+export const MarkdownContentDisplay: React.FC<MarkdownContentDisplayProps> = ({
+  content,
+}) => {
+  return <ReactMarkdown>{content.value}</ReactMarkdown>;
 };
